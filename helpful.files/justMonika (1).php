a4fa0cab4e558117a8761f304c44bc78ed5ee400
Isaac Schubert <greenace14@gmail.com>
2025-06-06 19:02:29 +0700

    remove

--- helpful.files/justMonika (1).php
+++
@@ -1,6 +0,0 @@
-<?php 
-include("glist.php");
-echo('Just Monika here');
-?>
-
-<img src="/img/gloomh.jpg" alt="Gloomhaven1" >
--- session_write.php
+++
@@ -1,5 +0,0 @@
-<?
-session_start();
-
-if (isset($_GET['session_name'])) {$_SESSION['session_name'] = $_GET['session_name'];}
-?>
